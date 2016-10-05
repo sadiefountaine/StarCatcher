fd98e2ee32ed0f16f98e791ef7d829c98f3aaa26
Mercedes Fountaine <sadiefountaine@live.com>
2016-10-05 13:55:59 -0600

    Anthony's class

---
+++ Star Catcher2/Assets/CharacterFlip/CharacterFlipArt.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterFlipArt : MonoBehaviour
+{
+
+    public Transform characterArt;
+    public bool forward = true;
+
+    void FlipCharacter(KeyCode _keyCode)
+    {
+        switch (_keyCode)
+        {
+            case KeyCode.UpArrow: //or left or right arrows
+                print("up");
+                break;
+        }
+
+        characterArt.Rotate(0, 180, 0);
+        //print("forward");
+        //forward = _b;
+    }
+
+    void Start()
+    {
+        UserInput.UserInput += FlipCharacter;
+    }
+}
