d252cb99c33985d8bf304fd6f871890f870d1616
Mercedes Fountaine <sadiefountaine@live.com>
2016-10-24 14:39:06 -0600

    Star Cloning Beginnings

---
+++ Star Catcher2/Assets/Stars Scripts/CloneStar.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloneStar : MonoBehaviour {
+
+    public Transform[] spawnPoints;
+    public Transform star;
+    public float spawnFrequency = 1;
+
+    //just spawns until we tell it to stop
+    public bool canSpawnStars = true;
+
+    private int i = 0;
+
+	IEnumerator SpawnStars()
+    {
+        //just spawns until we tell it to stop
+        while (canSpawnStars)
+        {
+            //just spawns until we tell it to stop
+            i = Random.Range(0, spawnPoints.Length - 1);
+
+            print(spawnPoints.Length);
+            Instantiate(star, spawnPoints[i].position, Quaternion.identity); //Quaternion.identity means reset rotate values to zero on star's rotation.
+            yield return new WaitForSeconds(spawnFrequency);
+            //i++;
+        }
+        //return null;
+    }
+
+	void Start () {
+        StartCoroutine(SpawnStars());
+	}
+}
---
+++ Star Catcher2/Assets/Stars Scripts/DestroyStar.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyStar : MonoBehaviour {
+
+    public float endTime = 3;
+
+   /* IEnumerator EndStar()
+    {
+        yield return new WaitForSeconds(endTime);
+    }
+    */
+	void OnCollisionEnter ()
+    {
+        Destroy(gameObject, endTime);
+    }
+}
+//Add to star prefab
+//make the spawn points a child of each level piece and then just put the spawn point above the camera (where you can't see it)
+//Or you can make one spawn point, put it on the camera, raise it above the camera & then it spawns where you are. Put it slightly ahead of the camera.
