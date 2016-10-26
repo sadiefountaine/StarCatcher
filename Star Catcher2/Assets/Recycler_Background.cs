b4425a24b0b8874aaa24be166690d41c6f21d787
Mercedes Fountaine <sadiefountaine@live.com>
2016-10-26 09:03:52 -0600

    In Process of Figuring out Background Recycler

---
+++ Star Catcher2/Assets/Recycler_Background.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Recycler_Background : MonoBehaviour
+{
+
+    public static Action<Recycler_Background> RecycleAction;
+    public bool canBeRecycled = false;
+    public Transform mesh;
+
+    // Use this for initialization
+    void Start()
+    {
+        mesh = this.GetComponent<Transform>();
+        if (RecycleAction != null && canBeRecycled)
+            RecycleAction(this);
+    }
+    void OnTriggerEnter()
+    {
+        canBeRecycled = true;
+        Start();
+    }
+}
---
+++ Star Catcher2/Assets/StaticVars_Background.cs
@@ -0,0 +1,10 @@
+﻿
+public class StaticVars_Background
+{
+
+    public static float distance = 20;
+
+    public static float nextSectionPosition = 40;
+
+    public static float startPosition = nextSectionPosition;
+}
