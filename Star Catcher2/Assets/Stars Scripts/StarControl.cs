2da69166fad90a42e9136f36a82073bcde96f30e
Mercedes Fountaine <sadiefountaine@live.com>
2016-10-26 14:03:41 -0600

    Star Control Stuff
    Unfinished (I missed a couple of things in class and need to go over it in Git.

---
+++ Star Catcher2/Assets/Stars Scripts/StarControl.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarControl : MonoBehaviour {
+
+    public int forceTime = 10;
+
+    public float forceDuration = 0.1f;
+    //private bool canAddForce = true;
+    private Rigidbody rigid;
+    private float force;
+    private Vector3 forceVector;
+    public float forceRange = 10;
+    //private Vector3 torqueVector;
+
+    private float torque;
+    public float torqueRange = 2;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+	
+	IEnumerator RunRandomForce()
+    {
+        force = Random.Range(-forceRange, forceRange);
+        torque = Random.Range(-torqueRange, torqueRange); //he moved these two down into the while thing.
+        //while (canAddForce)
+        while (forceTime > 0)
+        {
+            yield return new WaitForSeconds(forceDuration);
+            forceVector.x = force;
+            torqueVector.z -= force/force;
+            rigid.AddTorque(torqueVector);
+            rigid.AddForce(forceVector);
+
+            forceTime--;
+        }
+    }
+
+    public float endTime = 3;
+
+    void OnCollisionEnter ()
+    {
+        //canAddForce = false;
+        Destroy(gameObject, endTime);
+    }
+	
+}
