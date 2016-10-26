fc67ee26e451bd3a0c408ec561dd20b024031f6f
Mercedes Fountaine <sadiefountaine@live.com>
2016-10-25 19:55:23 -0600

    Found a problem.
    The rabbit moves deeper into the land modules along the x axis so much so that he is behind all of the objects that he is supposed to be jumping over. This only happens after playing the game for a couple of minutes. I am in the process of finding what is causing the problem so I can fix.it.

--- Star Catcher2/Assets/Scripts/Level Scripts/StaticVars.cs
+++ Star Catcher2/Assets/Scripts/Level Scripts/StaticVars.cs
@@ -1,9 +1,9 @@
 ﻿
 public class StaticVars {
 
-    public static float distance = 47;
+    public static float distance = 46;
 
-    public static float nextSectionPosition = 65;
+    public static float nextSectionPosition = 105;
 
     public static float startPosition = nextSectionPosition;
 }
