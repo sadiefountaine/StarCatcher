bd06e094f6c225e3e7abcb2e6f7bd286178eceb8
Mercedes Fountaine <sadiefountaine@live.com>
2016-10-27 12:47:35 -0600

    Star Scripts work, trying to figure out background recycle

--- Star Catcher2/Assets/Stars Scripts/CloneStar.cs
+++ Star Catcher2/Assets/Stars Scripts/CloneStar.cs
@@ -6,7 +6,6 @@ public class CloneStar : MonoBehaviour {
     public Transform[] spawnPoints;
     public Transform star;
     public float spawnFrequency = 1;
-
     //just spawns until we tell it to stop
     public bool canSpawnStars = true;
 
@@ -19,8 +18,7 @@ public class CloneStar : MonoBehaviour {
         {
             //just spawns until we tell it to stop
             i = Random.Range(0, spawnPoints.Length - 1);
-
-            print(spawnPoints.Length);
+            //print(spawnPoints.Length);
             Instantiate(star, spawnPoints[i].position, Quaternion.identity); //Quaternion.identity means reset rotate values to zero on star's rotation.
             yield return new WaitForSeconds(spawnFrequency);
             //i++;
--- Star Catcher2/Assets/Stars Scripts/StarControl.cs
+++ Star Catcher2/Assets/Stars Scripts/StarControl.cs
@@ -4,36 +4,29 @@ using System.Collections;
 public class StarControl : MonoBehaviour {
 
     public int forceTime = 10;
-
     public float forceDuration = 0.1f;
-    //private bool canAddForce = true;
     private Rigidbody rigid;
-    private float force;
-    private Vector3 forceVector;
     public float forceRange = 10;
-    //private Vector3 torqueVector;
-
-    private float torque;
     public float torqueRange = 2;
+    private Vector3 forceVector;
+    private Vector3 torqueVector;
+    
 
     void Start()
     {
         rigid = GetComponent<Rigidbody>();
+        StartCoroutine(RunRandomForce());
     }
 	
 	IEnumerator RunRandomForce()
     {
-        force = Random.Range(-forceRange, forceRange);
-        torque = Random.Range(-torqueRange, torqueRange); //he moved these two down into the while thing.
-        //while (canAddForce)
         while (forceTime > 0)
         {
             yield return new WaitForSeconds(forceDuration);
-            forceVector.x = force;
-            torqueVector.z -= force/force;
+            forceVector.x = Random.Range(-forceRange, forceRange);
+            torqueVector.z -= Random.Range(-torqueRange, torqueRange);
             rigid.AddTorque(torqueVector);
             rigid.AddForce(forceVector);
-
             forceTime--;
         }
     }
@@ -42,7 +35,6 @@ public class StarControl : MonoBehaviour {
 
     void OnCollisionEnter ()
     {
-        //canAddForce = false;
         Destroy(gameObject, endTime);
     }
 	
