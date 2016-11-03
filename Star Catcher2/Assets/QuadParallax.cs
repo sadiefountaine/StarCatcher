73ff13e25ff465bf4089c97d991c26248bcd5368
Mercedes Fountaine <sadiefountaine@live.com>
2016-11-03 12:50:18 -0600

    Sky Parallaxing Beginings

---
+++ Star Catcher2/Assets/QuadParallax.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuadParallax : MonoBehaviour {
+
+    public float speed = 0.01f;
+	
+	void Start () {
+	
+	}
+	
+	
+	void Update ()
+    {
+        Vector2 offset = new Vector2(speed * Time.time, 0);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
