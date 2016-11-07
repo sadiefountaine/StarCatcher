42a95a8ba5c69cc5cf02077ee0d27bc82efdc104
Mercedes Fountaine <sadiefountaine@live.com>
2016-11-07 13:49:57 -0700

    Nav Mesh Agent Stuff

---
+++ Star Catcher2/Assets/Agent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Agent : MonoBehaviour {
+
+    private NavMeshAgent myAgent;
+    public Transform player;
+
+	// Use this for initialization
+	void Start () {
+        myAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        myAgent.destination = player.position;
+	
+	}
+}
