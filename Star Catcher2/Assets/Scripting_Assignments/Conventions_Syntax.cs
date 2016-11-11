eb5fde22c14c21ecaa2caba02d5effd7efcd7863
Mercedes Fountaine <sadiefountaine@live.com>
2016-11-11 12:54:49 -0700

    Catch Up Assignments Part 1

--- Star Catcher2/Assets/Scripting_Assignments/Conventions_Syntax.cs
+++ Star Catcher2/Assets/Scripting_Assignments/Conventions_Syntax.cs
@@ -1,15 +1,38 @@
-﻿using UnityEngine;
+﻿//The UnityEngine is a namespaces and the System.Collectiions is a name space.
+//They allow us to use the functions an classes that have already been created in that particular name space.
+using UnityEngine;
 using System.Collections;
+
+//Classes:
+//A class shows us all of the different functions, statements, and comments we have in a script.
 
 public class Conventions_Syntax : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	//Function:
+    //A function holds all of the instructions for this specific part of the script.
+
+    //Statments are single lines of code of simple instructions within functions.
+
+	void Start ()
+    {
+        Debug.Log("This is a statement. It will only be called once.");
+	    //Start only uses statments inside of it one time.
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
-	}
+	void Update ()
+    {
+        /*
+        Debug.Log("This will repeat until the game is stopped");
+        Debug.Log("Hey look, a block of code!");
+        Debug.Log("This block of code will also repeat into the infinities.");
+        */
+
+        //Update will run that statment inside of it over and over again for every frame.
+
+        //Comments are useful for making notes, giving directions, and helping during the debug stage.
+        //If you would like to comment out a large section of code, use the multi-line comment. An example of this is on the block of code above.
+
+        //When debuging, you can comment out issues to help figure out what is going wrong and to get your stuff back into working order.
+    }
 }
--- Star Catcher2/Assets/Scripting_Assignments/Variables.cs
+++ Star Catcher2/Assets/Scripting_Assignments/Variables.cs
@@ -3,13 +3,63 @@ using System.Collections;
 
 public class Variables : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public int stars = 5;
+    public int starFallSpeed = 10;
+    private int starSpeed;
+    int star = 8;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+
+    //Video Notes:
+
+    //VIDEO 1:
+    //The access modifier tells who can access this variable.
+    //public = exposed in unity & everyone can access it.
+    //private = not exposed in unity and only you can access it.
+
+    //After the access modifier is the modifiers.
+    //const = constant = value of local variable cannot be modified.
+
+    //Next is the Type = tells variable what type of data the variable will hold.
+    //Int = integer = full number value.
+    //float for a decible value.
+
+    //Identifyier = name for the variable (can be anything we want such as speed)
+    // can't use while or if or for (would contradict the API (automated programming interface))
+    // must begin with a letter or an underscore. Can contain letters, numbers, or punctuation, but only after the first letter.
+    //cannot contain spaces (such as max speed) instead you would type maxSpeed.
+    
+    // every identifier needs to be assigned to a data vaule. = is the assignment operator.
+    // then you need to give it a value or data.
+    //after that you have the semicolon. A vaurable is like a statment. It must end with a semicolon.
+
+    //public int speed = 1;
+
+
+    //VIDEO 2:
+
+    //Variables can be compared to boxes that contain information. You need a different box for each bit of information you have.
+    //Start with what type of box yuo would like (int (interger, meaning a whole number))
+    //Then name the box.
+    //Then semicolon the end
+    //or you can give the box a value to store and then the semicolon.
+
+    //type nameOfVariable = value;
+
+    //int myInts;
+    //or
+    //int myInt = 5;
+
+
+    
 }
