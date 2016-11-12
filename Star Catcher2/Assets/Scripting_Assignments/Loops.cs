f61038a0b7c399d85ae972bb2c34a7e2381faf6f
Mercedes Fountaine <sadiefountaine@live.com>
2016-11-11 23:55:29 -0700

    Made some script edits

--- Star Catcher2/Assets/Scripting_Assignments/Loops.cs
+++ Star Catcher2/Assets/Scripting_Assignments/Loops.cs
@@ -3,13 +3,66 @@ using System.Collections;
 
 public class Loops : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public int Wolf = 4;
+
+    void FindWolf()
+    {
+        for(int i = 0; i < 6; i++)
+        {
+            Wolf = Random.Range(2, 20);
+
+            if ()
+        }
+    }
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+
+    }
+
+    /*
+    private int enemyDistance = 0;
 	
+	void Start ()
+    {
+	    
 	}
 	
-	// Update is called once per frame
-	void Update () {
 	
+	void Update ()
+    {
+      
 	}
+
+    //a for loop allows us to loop a block of code for a certain amount of time.
+
+    void EnemySearch()
+    {
+        for (int i = 0; i < 5; i++) //there are semicolons inbetween these statements.
+        {
+            enemyDistance = Random.Range(1, 10);
+
+            //we will create single if statements not else or else if.
+            if(enemyDistance >= 8)
+            {
+                Debug.Log("An enemy is way over there!");
+            }
+            if(enemyDistance >= 4 && enemyDistance <= 7)
+            {
+                Debug.Log("That enemy is getting closer");
+            }
+            if(enemyDistance < 4)
+            {
+                Debug.Log("This enemy has terrible breath");
+            }
+
+            //this if set up makes it so the code isn't run through everything over and over, but just sees the statement and then goes back.
+        }
+    }
+    */
 }
