7ee2089ff96d4fc6ca9d8dcf43dbefcb8fe963f3
Mercedes Fountaine <sadiefountaine@live.com>
2016-11-14 14:11:46 -0700

    Interfaces
    Anthony pushed his stuff to Git so where you got lost you can take notes. He will go over this again too.

---
+++ Star Catcher2/Assets/INTERFACES/Scripts_Interface/Interfaces/AddPoints.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddPoints : MonoBehaviour, IAddPoints
+{
+    public int points
+    {
+        get
+        {
+            throw new NotImplementedException(); //this just throws an error that says your code is doing nothing. This is a placeholder where you put your code.
+        }
+        set
+        {
+            throw new NotImplementedException();
+        }
+    }
+	 public void OnTriggerEnter()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Star Catcher2/Assets/INTERFACES/Scripts_Interface/Interfaces/IPowerUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IPowerUp { //doesn't extend monodeveolop, not a class but an interfaces, and names start with I.
+
+    int power { get; set; } //this is a variable but its a property //get means i can get info about the power. //set means I can set the power. //If i leave off set then i can't set the power //if i leave of get then i can't get the power.
+
+    void Start(); //not declaring what start is, but saying start is required
+
+    void OnTriggerEnter(); //if im using the interface of power up every interface using powerup is going to require ontriggerenter no matter what.
+
+
+    //because it doesn't contain monodevelop, start and update no longer work inside it.
+
+    //Now we are going to implement this in HealthPowerUp.
+
+}
