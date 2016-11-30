b4d28ccd60809cd741aeac1746da15b972167019
Mercedes Fountaine <sadiefountaine@live.com>
2016-11-30 14:06:20 -0700

    Anthony's Class (got a few errors)

---
+++ Star Catcher2/Assets/MoreHandlers.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoreHandlers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        DelegatesIntro.EventString += ES;
+        DelegatesIntro.myEvent += ME;
+        DelegatesIntro.EventWithArgs += EA;
+        DelegatesIntro.EventReturnArgs += ERA;
+	}
+	
+    private string ES()
+    {
+        return "Dogs are better";
+    }
+
+    private string ME()
+    {
+        print("Cool beans");
+    }
+
+    private string EA(string obj)
+    {
+        print ("But I love " + obj);
+    }
+
+    private string ERA(string argl)
+    {
+        return "I love " + argl + "too";
+    }
+}
