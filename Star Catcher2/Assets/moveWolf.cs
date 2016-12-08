931c8cd07c3812c2eb13394b1affbf8a0b7c4d7f
Mercedes Fountaine <sadiefountaine@live.com>
2016-12-08 12:49:02 -0700

    Two Wolves Run Across Screen, Rabbit's Jump is Higher

---
+++ Star Catcher2/Assets/moveWolf.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveWolf : MonoBehaviour
+{
+
+    public float speed = 1;
+    private Vector3 tempPos;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        tempPos.x = speed * Time.deltaTime;
+        transform.Translate(tempPos);
+    }
+}
