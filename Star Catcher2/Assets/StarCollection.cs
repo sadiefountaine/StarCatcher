7240a027487200e18a608d271d7b77a3e38e8c0b
Mercedes Fountaine <sadiefountaine@live.com>
2016-12-09 23:37:52 -0700

    The score board actually counts now, but you still can't pick up stars.
    I have been trying to figure out how to make it so I can collect falling stars because the code I found only shows me how to collect objects that aren't moving (thus when I try it, my objects fall through the ground because they are using onTriggerEnter and turning stars into triggers makes it so the stars can't see the ground).

--- Star Catcher2/Assets/StarCollection.cs
+++ Star Catcher2/Assets/StarCollection.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class StarCollection : MonoBehaviour {
 
+    private int count;
+    public Text countText;
 	
 	void Start () {
-	
+        count = 0;
+        SetCountText();
 	}
 	
 	
@@ -18,7 +22,13 @@ public class StarCollection : MonoBehaviour {
         if (other.gameObject.CompareTag("Pick Up"))
         {
            other.gameObject.SetActive(false);
+           count = count + 1;
+           SetCountText();
         }
     }
 
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+    }
 }
